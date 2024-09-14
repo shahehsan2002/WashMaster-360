6166da13f3098a793aceb9c7bf6c6f71345bf26d
Shah Ehsan <shahehsan2002@gmail.com>
2024-09-14 19:19:10 +0600

    catchAsync utils created

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response } from'express'
 const app = express()
 
+// middleware
+app.use(express.json())
+
+// routes
+app.use("/api/user",userRoutes)
+
 app.get('/', (req :Request, res:Response) => {
   res.send('Hello World!')
 })
---
+++ src/module/user/user.route.ts
@@ -0,0 +1,4 @@
+import express from 'express'
+const router = express.Router();
+
+router.post ("/create-admin",userController.createAdmin)
